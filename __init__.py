32c43bc84844945f83bc7dfd368cb574fa5ae4a6
montou <37769293+montou@users.noreply.github.com>
2018-03-25 11:07:46 +0200

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,94 @@
+﻿import sys
+from os.path import dirname, abspath
+
+from adapt.intent import IntentBuilder
+from mycroft.skills.core import MycroftSkill
+from mycroft.util.log import getLogger
+import time
+import subprocess
+
+from mycroft.util.log import getLogger
+
+sys.path.append(abspath(dirname(__file__)))
+dr = __import__('dr')
+
+logger = getLogger(__name__)
+
+__author__ = 'tjoen'
+
+
+class DutchRadio(MycroftSkill):
+    def __init__(self):
+        super(DutchRadio, self).__init__('Dutch Radio')
+        self.dr = dr.DutchRadio()
+        self.process = None
+
+    def initialize(self):
+        logger.info('initializing DutchRadio')
+        self.load_data_files(dirname(__file__))
+        super(DutchRadio, self).initialize()
+
+        for c in self.dr.channels.keys():
+            self.register_vocabulary(c, 'ChannelKeyword')
+        intent = IntentBuilder('PlayChannelIntent' + self.name)\
+            .require('PlayKeyword')\
+            .require('ChannelKeyword')\
+            .build()
+        self.register_intent(intent, self.handle_play_channel)
+        intent = IntentBuilder('PlayFromIntent' + self.name)\
+            .require('PlayKeyword')\
+            .require('ChannelKeyword')\
+            .require('NameKeyword')\
+            .build()
+        self.register_intent(intent, self.handle_play_channel)
+
+    def before_play(self):
+        """
+           Stop currently playing media before starting the new. This method
+           should always be called before the skill starts playback.
+        """
+        logger.info('Stopping currently playing media if any')
+        #if self.process:
+        #    self.stop()
+
+    def play(self):
+        self.before_play()
+        self.speak_dialog('listening_to', {'channel': self.channel})
+        time.sleep(2)
+        stream_url = self.dr.channels[self.channel].stream_url
+        self.process = subprocess.Popen(['mpg123', stream_url])
+
+    def get_available(self, channel_name):
+        logger.info(channel_name)
+        if channel_name in self.dr:
+            logger.info('Registring play intention...')
+            return channel_name
+        else:
+            return None
+
+    def prepare(self, channel_name):
+        if self.process:
+            self.stop()
+        self.channel = channel_name
+
+    def handle_play_channel(self, message):
+        logger.debug( message.data )
+        c = message.data.get('ChannelKeyword')
+        self.prepare(c)
+        self.play()
+
+    def stop(self, message=None):
+        logger.info('Handling stop request')
+        if self.process:
+            self.process.terminate()
+            self.process.wait()
+            self.process = None
+            self.channel = None
+
+    def handle_currently_playing(self, message):
+        if self.channel is not None:
+            self.speak_dialog('currently_playing', {'channel': self.channel})
+
+
+def create_skill():
+    return DutchRadio()
---
+++ dr.py
@@ -0,0 +1,38 @@
+﻿import csv
+
+from collections import OrderedDict
+
+class Channel():
+    def __init__(self, name=None, id=None, stream_url=None):
+        self.name = name
+        self.id = id
+        self.stream_url = stream_url
+
+
+class DutchRadio():
+    def __init__(self):
+        indx = 0
+        r = open('/opt/mycroft/skills/skill-dutch-radio/radio.csv', 'r')
+        reader = csv.reader(r, delimiter=',')
+        channellist = list(reader)
+        self.channels = {}
+        if r:
+            for c in channellist:
+                name =  channellist[indx][0].strip()
+                id = indx
+                stream_url = channellist[indx][1].strip()
+                self.channels[name] = Channel(name, id, stream_url)
+                indx += 1 
+
+    def __contains__(self, channel):
+        return channel in self.channels
+
+    def get_next(current):
+        keys = channels.keys()
+        pos = 0
+        for k in keys:
+            if k == current:
+                break
+            pos += 1
+        if pos < len(keys):
+            return self.channels(keys[pos])
